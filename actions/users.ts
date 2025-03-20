536ed11e29b35be149152151737250c631d9ed6d
Jamin <jaminwebdev@gmail.com>
2025-03-20 13:29:53 -0500

    user actions

---
+++ actions/users.ts
@@ -0,0 +1,58 @@
+'use server';
+
+import { createClient } from '@/auth/server';
+import { handleError } from '@/lib/utils';
+
+export const loginAction = async (email: string, password: string) => {
+  try {
+    const { auth } = await createClient();
+
+    const { error } = await auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) throw error;
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const logOutAction = async () => {
+  try {
+    const { auth } = await createClient();
+
+    const { error } = await auth.signOut();
+    if (error) throw error;
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const signUpAction = async (email: string, password: string) => {
+  try {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) throw error;
+
+    const userId = data.user?.id;
+    if (!userId) throw new Error('Error signing up');
+
+    const { error: userCreationError } = await supabase
+      .from('ai_notes_user')
+      .insert([{ id: userId, email }]);
+
+    if (userCreationError) throw userCreationError;
+
+    return { errorMessage: null };
+  } catch (error) {
+    return handleError(error);
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import { getUser } from '@/auth/server';
 
-function Home() {
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+async function Home({ searchParams }: Props) {
+  const noteIdParam = (await searchParams).noteId;
+  const user = await getUser();
+
   return <div>Home Page</div>;
 }
 
