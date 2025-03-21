cf54a7eedb1140217c5254aa36b385a7bd47a688
Jamin <jaminwebdev@gmail.com>
2025-03-21 14:25:23 -0500

    revalidate notes client-side

--- components/NoteList.tsx
+++ components/NoteList.tsx
@@ -1,39 +1,49 @@
 'use client';
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { Note } from '@/types/types';
 import Link from 'next/link';
-import { formatDistanceToNow } from 'date-fns';
+import { format } from 'date-fns';
 import DeleteNoteButton from './DeleteNoteButton';
+import { useGetAllNotes } from '@/hooks/notehooks';
 
 type Props = {
   notes: Note[];
 };
 
 function NoteList({ notes }: Props) {
+  const { data: fetchedNotes, error: fetchedNotesError } =
+    useGetAllNotes(notes);
+
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {notes.map(note => (
+      {fetchedNotes.map(note => (
         <Link
           key={note.id}
           href={`/notes/${note.id}`}
           className="group relative block"
         >
-          <Card className="h-full transition-colors hover:bg-muted/50">
+          <Card className="h-full transition hover:bg-muted/50 hover:scale-105">
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-lg font-semibold">
                 {note.title}
               </CardTitle>
+              <CardDescription>
+                <p className="mt-2 text-xs text-muted-foreground">
+                  {format(new Date(note.updated_at), 'MM/dd/yyyy')}
+                </p>
+              </CardDescription>
               <DeleteNoteButton noteId={note.id} />
             </CardHeader>
             <CardContent>
               <p className="text-sm text-muted-foreground line-clamp-3">
                 {note.body}
-              </p>
-              <p className="mt-2 text-xs text-muted-foreground">
-                {formatDistanceToNow(new Date(note.updated_at), {
-                  addSuffix: true,
-                })}
               </p>
             </CardContent>
           </Card>
